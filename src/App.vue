76f439a23cc8d28ed2dd94d435f879592a7d3e1c
Ellis Tran <ellisktran@gmail.com>
2023-09-10 17:35:34 -0400

    feature: connect to gas prices api, implement search for state, and get all usa prices per state

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <!-- <button @click="getWeatherData">Get me the data</button> -->
+    <form @submit.prevent="getWeatherData">
+    <label>Please search for a State using their Postal Code</label>
+    <input type="text" v-model="cityToSearch">
+    <button>Search</button>
+    </form>
+    <ul>
+      <li v-for="city in cities" :key="city.lowerName">{{ city.name }}</li>
+    </ul>
+    <ul>
+      <li v-for="state in states" :key="state.name">{{ state.name }}</li>
+    </ul>
+
+    <button @click.prevent="getGasDataForUSACities">Search for All USA Prices</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      cities: [],
+      states: [],
+      cityToSearch: '',
+    }
+  },
+  methods: {
+    async getWeatherData() {
+      await fetch(`https://api.collectapi.com/gasPrice/stateUsaPrice?state=${this.cityToSearch}`, {
+        headers: {
+          "Content-type": "application/json",
+          "authorization": "apikey 4T56yIcfcwAQ1oTigoZ5a6:4hcmA0s2a8smtj57yWToh4"
+        }
+      }).then(response => response.json()).then(result => { this.cities.push(...result.result.cities); console.log(result) });
+    },
+    async getGasDataForUSACities() {
+      await fetch(`https://api.collectapi.com/gasPrice/allUsaPrice`, {
+        headers: {
+          "Content-type": "application/json",
+          "authorization": "apikey 4T56yIcfcwAQ1oTigoZ5a6:4hcmA0s2a8smtj57yWToh4"
+        }
+      }).then(response => response.json()).then(result => { this.states.push(...result.result); console.log(result) });
+    }
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+import CityGas from './CityGas.vue';
+import StateGas from './StateGas.vue';
+
+const app = createApp(App);
+
+app.component('city-gas', CityGas)
+app.component('state-gas', StateGas);
+
+app.mount('#app');
